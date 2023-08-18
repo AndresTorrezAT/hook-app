dfabf25d632d87bed771d6d9cb3f0648aa20b5aa
Andres <andres.torrez.work@gmail.com>
2023-08-18 17:38:41 -0400

    useContext Finalizado

--- src/09-useContext/context/UserProvider.jsx
+++ src/09-useContext/context/UserProvider.jsx
@@ -1,15 +1,21 @@
+import { useState } from "react"
 import { UserContext } from "./UserContext"
 
-const user = {
-    id: 123,
-    name: 'Fernando Herrera',
-    email: 'andres@google.com',
-}
+// const user = {
+//     id: 123,
+//     name: 'Fernando Herrera',
+//     email: 'andres@google.com',
+// }
 
 export const UserProvider = ({ children }) => {
+
+  const [user, setUser] = useState();
+
   return (
-    <UserContext.Provider value={{ hola: 'Mundo', user: user }}>
+    // <UserContext.Provider value={{ hola: 'Mundo', user: user }}>
+    <UserContext.Provider value={{ user, setUser }}>
         { children }
     </UserContext.Provider>
   )
+  
 }
