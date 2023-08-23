3ba0cd6403137a2beab7474618a30d195bf991ab
Andres <andres.torrez.work@gmail.com>
2023-08-23 15:52:55 -0400

    Fin de Testing

--- tests/09-useContext/MainApp.test.jsx
+++ tests/09-useContext/MainApp.test.jsx
@@ -8,13 +8,28 @@ describe('Pruebas en <MainApp/>', () => {
     
     test('debe de mostrar el HomePage', () => {
 
+        // hay qye proveerle los hooks necesarios en este caso BrowserRouter pero para test se llama MemoryRouter
+
         render( 
             <MemoryRouter>
                 <MainApp/> 
             </MemoryRouter>
         );
-        expect( screen.getByText('HomePage') ).toBeTruthy();
-        screen.debug();
+        expect( screen.getByText('HomePage') ).toBeTruthy(); // que exista el home page
+        // screen.debug();
 
     });
+
+    test('debe de mostrar el LoginPage', () => {
+
+        render( 
+            <MemoryRouter initialEntries={['/login']}>
+                <MainApp /> 
+            </MemoryRouter>
+        );
+        expect( screen.getByText('LoginPage') ).toBeTruthy(); // que exista el home page
+        // screen.debug();
+
+    });
+
 });
